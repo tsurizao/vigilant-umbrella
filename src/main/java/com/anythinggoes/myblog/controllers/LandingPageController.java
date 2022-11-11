e44de8804c44accd6f9eb67e62a25f7ad6cb43d4
Chase Martinez <chasemartinez22@gmail.com>
2022-11-11 16:02:05 -0600

    Commented out spring-boot-starter-data-jpa to remove issue with loading index page without a database

--- src/main/java/com/anythinggoes/myblog/controllers/LandingPageController.java
+++ src/main/java/com/anythinggoes/myblog/controllers/LandingPageController.java
@@ -2,10 +2,12 @@ package com.anythinggoes.myblog.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class LandingPageController {
     @GetMapping("/")
+    @ResponseBody
     public String showLandingPage(){
         return "index";
     }
